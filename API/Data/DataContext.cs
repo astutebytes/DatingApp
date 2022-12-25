0359fc34d0d605af9525c72d287b0c6d49dabfe0
Astute Bytes <usmanuumer@outlook.com>
2022-12-25 23:03:59 +0500

    Added likes functionality

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,27 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(k => k.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(k => k.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
 
--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -127,4 +127,14 @@ export class MemberService {
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {});
+  }
+
+  getLikes(predicate: string) {
+    return this.http.get<Member[]>(
+      this.baseUrl + 'likes?predicate=' + predicate
+    );
+  }
 }
